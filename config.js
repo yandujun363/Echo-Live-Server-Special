a8554b688d1c2a0bc5f7e3ef71f8c440e9547189
yangdujun363 <yangdujun363@outlook.com>
2025-06-01 02:15:58 +0800

    修改配置加载代码

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
+//使用AJAX加载配置文件
 const config = $.ajax({
     type:"get",
     url:"config.json",
-    async:false,
-    cache:false,
+    async:false,//同步模式，方便获取数据
+    cache:false,//禁用缓存
+    dataType:"json"
 }).responseJSON;
